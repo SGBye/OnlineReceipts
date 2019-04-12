8c41701f0a2bb6be612697367ed58705f47d99ff
SGBye <ctac1995@gmail.com>
2019-04-13 00:31:27 +0300

    123

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -39,13 +39,11 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'accounts',
     'receipt',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
