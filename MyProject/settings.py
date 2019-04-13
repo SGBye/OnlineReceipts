5bc0aa6f46d7ff172f148d31d20702fb4c05039e
SGBye <ctac1995@gmail.com>
2019-04-13 12:36:14 +0300

    allowed

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'kw81bgyg@izow+l0p3+!sj-8+2z+m8e0gpnh05ri@_nke(m5%l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'shielded-shore-48386.herokuapp.com']
 
 # Application definition
 
